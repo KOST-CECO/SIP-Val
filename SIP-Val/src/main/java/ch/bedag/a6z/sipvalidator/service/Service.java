76f0d9f0ac2cf14c54ac81c89dbe35efddee2d90
Chlara <chlara@gmx.ch>
2012-07-19 16:40:24 +0200

    Version 0.3.0 =============  - GPL2 durch GPL3 ersetzt  - Usage-Meldung verbessert  - Kommentar: @author razm Daniel Ludin, Bedag AG @version 2.0 auf Version 0.2.0 gesetzt  - Update zip64 von 1.00 auf 1.02 (1.02 Cleaned up the code, the build script and added "or later" to the GPL).  - Anpassungen der Fehlermeldungen 2a, 2b und 2c mit verständlicheren Meldungen  - Fehlermeldung in 2d "Keine Referenzierung in metadata.xml/paket/inhaltsverzeichnis/content" als Kommentar maskiert,    weil diese Dateien kassiert wurden und es keinen Fehler ist, respektive nicht ausgegeben werden sollen  - französische Kopie von src/main/resources/messages.properties als fr_messages.properties erstellt  - SIP-Name Start-Notiz in src/main/resources/messages.properties/message.sipvalidation integriert  - MESSAGE_SIPVALIDATION mit sipDatei ergänzt in SipValidator.java (logInfo & logError)  - getPathToDroidSignatureFile anstelle getNameOfDroidSignatureFile (original maskiert), da    die fehlerfreie Kompilierung bei KOST ansonsten nicht möglich war  - Struktur an KOST angepasst kostceco.bento.sipval anstelle bedag.a6z.sipvalidator  - gpl2 Notiz in ale src intergriert  - gpl2 Start-Notiz in src/main/resources/messages.properties/message.sipvalidation integriert  - ü in src/main/resources/messages.properties/message.sipvalidation normalisiert  - Unzulaessiges Format des SIP-Namen anstelle Dateinamen in src/main/resources/messages.properties/message.sipvalidation  - exitValue in JhoveServiceImpl als Kommentar maskiert, weil diese nicht verwendet wird  - exitValue in PdftronServiceImpl als Kommentar maskiert, weil diese nicht verwendet wird

--- SIP-Val/src/main/java/ch/bedag/a6z/sipvalidator/service/Service.java
+++
@@ -1,9 +0,0 @@
-package ch.bedag.a6z.sipvalidator.service;
-
-import ch.bedag.a6z.sipvalidator.logging.MessageConstants;
-
-public interface Service extends MessageConstants {
-
-    // Marker Interface
-    
-}
